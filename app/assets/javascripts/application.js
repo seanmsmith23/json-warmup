a58db17d12825deec164aa83d4843c099aa1dfb7
Sean Smith <seansmith1020@gmail.com>
2014-08-26 09:27:52 -0600

    table is filled

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -44,6 +44,19 @@ $(document).ready(function () {
       }
     ]
   };
+
+  var addRow = function(owner, dogName, favFood, favToy){
+    $('table').append("<tr><td>" + owner + "</td><td>" + dogName + "</td><td>" + favFood + "</td><td>" + favToy + "</td></tr>")
+  };
+
+ var something = gschoolDogs["dogs"]
+
+  for (index = 0; index < something.length; ++index) {
+    addRow(something[index]["owner"], something[index]["name"], something[index]["preferences"][0]["favoriteFood"], something[index]["preferences"][1]["favoriteToy"])
+  }
+
+
+
   // Write come code to add the dogs to the table !!!!
 });
 
